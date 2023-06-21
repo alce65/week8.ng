aa87bc5b640ce4bb6a4b6282cc5ad62f1bfc80f5
Alejandro Cerezo <alce65@hotmail.es>
2023-06-21 10:27:05 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'isd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'week8.ng';
+}
